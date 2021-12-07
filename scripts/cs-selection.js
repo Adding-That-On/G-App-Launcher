5ccfbe95515c23b199b4ff032f9b5e0a9efe71cd
Minionguyjpro <66115754+Minionguyjpro@users.noreply.github.com>
2021-12-07 10:29:22 +0100

    Add extension files

---
+++ scripts/cs-selection.js
@@ -0,0 +1,1 @@
+"use strict";window.getSelection().toString();
---
+++ scripts/webext-flags.js
@@ -0,0 +1,1 @@
+"use strict";var webextFlags={default_action_icon:"alg",cj_landing_path:"g-app-launcher",gsuite_apps_support:!0,modify_google_applauncher:"choice",use_color_override:!0,inline_drag_and_drop:!0,include_alg_welcome:!0,custom_action_icons:["gsa_nobg","gsa_grey","app_launcher"]};
