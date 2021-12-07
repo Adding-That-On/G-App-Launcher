e0976e508938a3174e7eca22cd73ea96214b0b13
Minionguyjpro <66115754+Minionguyjpro@users.noreply.github.com>
2021-12-07 12:29:39 +0100

    Create CreateZIP.ps1

---
+++ .github/workflows/CreateZIP.ps1
@@ -0,0 +1,5 @@
+$source = "D:\G-App-Launcher\G-App-Launcher\"
+$destination = "D:\G-App-Launcher\G-App-Launcher\G-App-Launcher_latest.zip"
+If(Test-path $destination) {Remove-item $destination}
+Add-Type -assembly "system.io.compression.filesystem"
+[io.compression.zipfile]::CreateFromDirectory($Source, $destination) 
